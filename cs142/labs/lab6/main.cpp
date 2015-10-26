f6a8cf209c5f52827626da81eb927071ad455a58
Ethan Durrant <emd519@yahoo.com>
2015-10-26 15:11:40 -0600

    added more 2D array functions to lab6

--- cs142/labs/lab6/main.cpp
+++ cs142/labs/lab6/main.cpp
@@ -6,20 +6,35 @@
 #include <stdlib.h>
 
 using namespace std;
+
 #define TEMP_ARR_X 20
 #define TEMP_ARR_Y 20
 
-float old_temps[TEMP_ARR_Y][TEMP_ARR_X];
-float new_temps[TEMP_ARR_Y][TEMP_ARR_X];
-
 int init_temp_arr(float temps[TEMP_ARR_Y][TEMP_ARR_X]);
 int print_temp_arr(float temps[TEMP_ARR_Y][TEMP_ARR_X]);
+void copy_temp_arr(float from_temps[TEMP_ARR_Y][TEMP_ARR_X],
+                    float to_temps[TEMP_ARR_Y][TEMP_ARR_X]);
+
+float calc_temp_arr(float prev_temps[TEMP_ARR_Y][TEMP_ARR_X],
+                    float calc_temps[TEMP_ARR_Y][TEMP_ARR_X]);
 
 int main(){
-    init_temp_arr(old_temps);
+    float old_temps[TEMP_ARR_Y][TEMP_ARR_X];
+    float new_temps[TEMP_ARR_Y][TEMP_ARR_X];
     init_temp_arr(new_temps);
+    copy_temp_arr(new_temps, old_temps);
+
+    //testing printing
+    print_temp_arr(old_temps);
     print_temp_arr(new_temps); 
+
     return 0;
+}
+
+//returns the value of the greatest change
+float calc_temp_arr(float prev_temps[TEMP_ARR_Y][TEMP_ARR_X],
+                    float calc_temps[TEMP_ARR_Y][TEMP_ARR_X]){
+
 }
 
 int init_temp_arr(float temps[TEMP_ARR_Y][TEMP_ARR_X]){
@@ -32,6 +47,15 @@ int init_temp_arr(float temps[TEMP_ARR_Y][TEMP_ARR_X]){
             } else {
                 temps[y][x] = 0.0;
             }
+        }
+    }
+}
+
+void copy_temp_arr(float from_temps[TEMP_ARR_Y][TEMP_ARR_X],
+                    float to_temps[TEMP_ARR_Y][TEMP_ARR_X]){
+    for(int y=0;y<TEMP_ARR_Y;y++){
+        for(int x=0;x<TEMP_ARR_X;x++){
+            to_temps[y][x] = from_temps[y][x]; 
         }
     }
 }
