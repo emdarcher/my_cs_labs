7f939ed330830475f3c0e0f3656587ddec889738
Ethan Durrant <emd519@yahoo.com>
2015-10-26 09:48:05 -0600

    starting lab5

---
+++ cs142/labs/lab6/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+float old_temps[20][20];
+float new_temps[20][20];
+
+int main(){
+
+    return 0;
+}
+
