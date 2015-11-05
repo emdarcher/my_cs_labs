6ebdf846275eb26ab974a21b33afb25bbce79819
Ethan Durrant <emd519@yahoo.com>
2015-11-05 14:54:23 -0700

    completed part1 of lab7

--- cs142/labs/lab7/main.cpp
+++ cs142/labs/lab7/main.cpp
@@ -9,8 +9,114 @@
 
 using namespace std;
 
-int main(){
+#define BUFF_SIZE 128
 
+#define ERRORED     1
+#define NO_ERR      0
+
+#define OPT_MIN     1
+#define OPT_MAX     6
+
+int get_int(int *in_int);
+int run_menu(void);
+void print_restaurants(vector<string> restaurants){
+    for(int i=0;i<restaurants.size();i++){
+        printf("%s", restaurants[i].c_str());
+        if(i != restaurants.size() - 1){
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+
+#define INIT_RESTAURANT_NUM 8 
+void init_restaurants(vector<string>& restaurants){ 
+    string rest[] = {
+        "Five Guys",
+        "SubWay",
+        "Chick-Fil-A",
+        "In-n-Out",
+        "Wagamama",
+        "Taco Bell",
+        "KFC",
+        "Olive Garden"
+    };
+    for(int i=0;i<INIT_RESTAURANT_NUM;i++){
+        restaurants.push_back(rest[i]); 
+    }
+}
+
+enum {
+    DISPLAY_ALL_RESTAURANTS = 1,
+    ADD_A_RESTAURANT,
+    REMOVE_A_RESTAURANT,
+    SHUFFLE_THE_VECTOR,
+    BEGIN_THE_TOURNAMENT,
+    QUIT_THE_PROGRAM
+};
+
+int main(){
+    vector<string> restaurants;
+    init_restaurants(restaurants);
+
+    int option = 0;    
+    uint8_t main_loop_var = 1;
+    while(main_loop_var){
+        option = run_menu();
+        int error = 0;
+        //handle the different option cases
+        switch (option) {
+            case DISPLAY_ALL_RESTAURANTS:
+                print_restaurants(restaurants);
+                break;
+            case QUIT_THE_PROGRAM:
+                //printf("GOODBYE!\n");
+                return 0;
+                break;
+        }
+    }
     return 0;
 }
 
+int get_int(int *in_int){
+    char buf[BUFF_SIZE];
+    int tmp_int;
+    char end_char;
+    fgets( buf, sizeof(buf), stdin ); 
+    //check for integer input and ending character
+    if(sscanf( buf, "%i%c", &tmp_int, &end_char ) == 2){
+        //check if integer is followed by a newline character
+        if(end_char == '\n'){
+            *in_int = tmp_int;
+            return NO_ERR;
+        }
+    }
+    return ERRORED;
+}
+
+int run_menu(void){
+
+    int opt = 0;
+    int loop_var = 1;
+    while(loop_var){
+        printf("MENU: Please select one of the following options:\n"
+                "  1 - Display all restaurants\n"
+                "  2 - Add a restaurant\n"
+                "  3 - Remove a restaurant\n"
+                "  4 - Shuffle the vector\n"
+                "  5 - Begin the tournament\n"
+                "  6 - Quit the program\n"
+                "Enter your selection now: ");
+        //check for integer input
+        if(get_int( &opt) == NO_ERR){
+            if((opt >= OPT_MIN) && (opt <= OPT_MAX)){
+                break;
+            }
+        }
+        printf("INVALID SELECTION. Please enter an int between %i and %i\n\n",
+                    OPT_MIN, OPT_MAX);
+    }
+    return opt;
+
+}
