09d43a833c52e4d9f6043ba2015372af374bb940
Ethan Durrant <emd519@yahoo.com>
2015-11-05 10:04:04 -0700

    starting on lab7 code

---
+++ cs142/labs/lab7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
