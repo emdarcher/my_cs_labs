442283bcd7096e409bcab167b8b44fe49cf64529
Ethan Durrant <emd519@yahoo.com>
2016-01-04 11:17:12 -0700

    creating base files for labs in cs235

---
+++ cs235/projects/basefiles/main.cpp
@@ -0,0 +1,17 @@
+//this has to be defined for this to compile in Visual Studio
+#define _CRT_SECURE_NO_DEPRECATE
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
