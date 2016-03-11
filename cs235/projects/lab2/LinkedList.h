49df738ea30f5914bc823cfca9d4d81c9010b774
Ethan Durrant <emd519@yahoo.com>
2016-03-10 18:02:16 -0700

    working on implementing the linked list

--- cs235/projects/lab2/LinkedList.h
+++ cs235/projects/lab2/LinkedList.h
@@ -6,6 +6,26 @@
 
 template<typename T>
 class LinkedList : public LinkedListInterface<T> {
+    private:
+        class Node {
+            public:
+                T data;
+                Node* next;
+                Node(const T& data_item, Node* next_ptr = NULL); 
+        };
+        Node* head;
+        Node* tail;
+        Node* find_node(T value);
+    public:
+        LinkedList(void);
+        virtual ~LinkedList(void);
+        virtual void insertHead(T value);
+        virtual void insertTail(T value);
+        virtual void insertAfter(T value, T insertionNode);
+        virtual void remove(T value);
+        virtual void clear();
+        virtual T at(int index);
+        virtual int size();
 
 };
 
